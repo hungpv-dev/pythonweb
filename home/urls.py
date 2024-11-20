7b127060d0017e6c640e571c8f15cb33a39dc897
hungpv <phamvanhung.dev@gmail.com>
2024-11-20 10:32:20 +0700

    tach api ra rieng

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('posts', views.post, name='posts'),
-    path('posts/<int:id>/', views.post_show, name='post_show'),
-    path('run-crawl/', views.run_crawl, name='run_crawl'),
-    path('api/posts/', views.api_posts, name='api_posts'),
-    path('api/posts/<int:id>/', views.api_post_detail, name='api_post_detail'),
+    path('api/', include('home.api.urls')),
+    # path('run-crawl/', views.run_crawl, name='run_crawl'),
+    # path('api/posts/', views.api_posts, name='api_posts'),
+    # path('api/posts/<int:id>/', views.api_post_detail, name='api_post_detail'),
 ]
